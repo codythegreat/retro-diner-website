fa8f8626a4d6e47d8a3f51bf8090f11834e3f16d
codythegreat <maxiecody@gmail.com>
2019-01-03 13:14:04 +0000

    bootstrap navbar, adding image text

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-  $("#hidden").hover(function () {
-      $(this).css("color", "black");
-    },
-    function () {
-      $(this).hide();
-    });
-});
+  $("#tv-animation").click(function () {
+    let ctx = document.getElementById("tv-animation").getContext("2d");
+    ctx.moveTo(10, 10);
+    ctx.font = "30px arial white";
+    ctx.strokeText('Hello World', 10, 50);
+
+  })
+})
