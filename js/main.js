9def6e83e5bc886473ef11bc4ec97db95fa3a563
EC2 Default User <maxiecody@gmail.com>
2019-01-05 20:46:14 +0000

    finished with styling

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $("#tv-animation").click(function () {
-    let ctx = document.getElementById("tv-animation").getContext("2d");
-    ctx.moveTo(10, 10);
-    ctx.font = "30px arial white";
-    ctx.strokeText('Hello World', 10, 50);
-
-  })
-})
